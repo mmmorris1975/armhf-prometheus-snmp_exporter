2383132e0a8c52e4a5056472961a036664ba7a12
Mike Morris <michael.m.morris@gmail.com>
2017-06-18 13:08:25 -0500

    Add build script for the `generator` tool and generate a config file
    Build the generator tool for the release of snmp exporter we're building
    the image for.  Also build a config file for the exporter, but use a
    custom generator.yml to remove the vendor-specific stuff which is causing
    errors.

---
+++ build-config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+REPO_DIR=$PWD
+BUILD_PATH='github.com/prometheus/snmp_exporter/generator'
+SNMP_EXPORTER_VERSION="v0.4.0"
+
+export GOPATH=$REPO_DIR/go
+mkdir -p $GOPATH
+
+go get -d -u $BUILD_PATH
+
+cd $GOPATH/src/$BUILD_PATH
+git checkout $SNMP_EXPORTER_VERSION
+go build -v
+
+cp generator $REPO_DIR/
+
+# Build using our own generator.yml to get rid of the vendor-specific noise
+cd $REPO_DIR
+./generator generate
